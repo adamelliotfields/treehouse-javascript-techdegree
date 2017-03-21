8ea65aa259ec75ae857f7fe5d10158dc1fe18e7e
adamelliotfields <adam.elliot.fields@gmail.com>
2017-03-21 09:21:18 -0400

    All requirements working.

--- 01-random-quote-generator/README.md
+++ 01-random-quote-generator/README.md
@@ -1,1 +1,12 @@
 # Random Quote Generator
+
+### Requirements
+*  Must have an array named `quotes` of at least 5 objects
+*  Each quote object must have `quote`, `source`, `citation`, `date`, and `tags` properties.
+*  Must have a `getRandomQuote()` function that returns a random object from the `quotes` array.
+*  `getRandomQuote()` must not return a duplicate quote until all quotes have been viewed at least once.
+*  Must have a `printQuote()` function that invokes `getRandomQuote()` and rewrites the HTML of the `quote-box` div.
+*  `printQuote()` must also change the background color to a random RGB value.
+*  `printQuote()` must be bound to a `button` click event handler as well as the `Window.setInterval()` method.
+*  When `printQuote()` is invoked, it must log the quote to the console as well.
+*  Code must be well commented!
---
+++ 01-random-quote-generator/js/script.js
@@ -0,0 +1,95 @@
+// Note: this is using some ES2015 syntax - it is valid if you're unfamiliar
+
+// All quotes are objects stored in the quotes array
+let quotes = [
+  {
+    quote: 'The ability to destroy a planet is insignificant next to the power of the force.',
+    source: 'Darth Vader',
+    citation: 'Star Wars: Episode IV - A New Hope',
+    tags: 'scary',
+    number: 1,
+    year: 1977,
+  },
+  {
+    quote: 'Try not. Do or do not. There is no try.',
+    source: 'Yoda',
+    citation: 'Star Wars: Episode V - The Empire Strikes Back',
+    tags: 'motivational',
+    number: 2,
+    year: 1980,
+  },
+  {
+    quote: 'You truly belong with us among the clouds.',
+    source: 'Lando Calrissian',
+    citation: 'Star Wars: Episode VI - The Empire Strikes Back',
+    tags: 'charming',
+    number: 3,
+    year: 1980,
+  },
+  {
+    quote: 'Just for once, let me look at you with my own eyes.',
+    source: 'Darth Vader',
+    citation: 'Star Wars: Episode VI - Return of the Jedi',
+    tags: 'sad',
+    number: 4,
+    year: 1983,
+  },
+  {
+    quote: 'It\'s a trap!',
+    source: 'Admiral Ackbar',
+    citation: 'Star Wars: Episode VI - Return of the Jedi',
+    tags: 'famous',
+    number: 5,
+    year: 1983,
+  }
+];
+
+// Displayed quotes will be appended to the viewedQuotes array
+let viewedQuotes = [];
+
+// Random number generator used for getRandomQuote() and getRandomColor()
+function randomNumber(num) {
+  return Math.floor(Math.random() * num);
+}
+
+// Pick a random quote object from the quotes array
+function getRandomQuote() {
+  return quotes[randomNumber(quotes.length)];
+}
+
+// Pick a random color for the page background
+function getRandomColor() {
+  return `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
+}
+
+// Change quote and background color of page
+function printQuote() {
+  // Invoke getRandomQuote() and store return value in quote variable
+  const quote = getRandomQuote();
+  // Log the quote number and quote to console
+  console.log(`${quote.number} ${quote.quote}`);
+  // Append the viewed quote to the viewedQuotes array and splice from the quotes array
+  viewedQuotes.push(quote);
+  quotes.splice(quotes.indexOf(quote), 1);
+  // Change quote-box div HTML each time printQuote() is invoked
+  document.getElementById('quote-box').innerHTML =  `
+    <p class="quote">${quote.quote}</p>
+    <p class="source">${quote.source}
+      <span class="citation">${quote.citation}</span>
+      <span class="year">${quote.year}</span>
+    </p>
+  `;
+  // Change body background color each time printQuote() is invoked
+  document.getElementsByTagName('body')[0].style = `background: ${getRandomColor()}`;
+  // Once quotes array has been emptied, replace with the contents of viewedQuotes and empty viewedQuotes
+  if (quotes.length === 0) {
+    quotes = viewedQuotes;
+    viewedQuotes = [];
+  }
+}
+
+// invoke printQuote() every 5 seconds
+setInterval(printQuote, 5000);
+
+// button click handler to invoke printQuote()
+document.getElementById('loadQuote').addEventListener('click', printQuote);
